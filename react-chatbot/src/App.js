bd6e78531750b72c59ff7ff237e132cbb61b0c5f
FonzyLima <71457986+FonzyLima@users.noreply.github.com>
2021-11-29 15:49:33 +0800

    Chatbot with Lourdene

--- react-chatbot/src/App.js
+++ react-chatbot/src/App.js
@@ -1,12 +1,20 @@
 import Chatbot from 'react-chatbot-kit';
-
+import 'react-chatbot-kit/build/main.css';
 import React from "react";
 import './App.css';
+
+import MessageParser from './MessageParser';
+import ActionProvider from './ActionProvider';
+import config from './config';
 
 function App() {
   return (
     <div className="App">
-      <Chatbot/>
+      <Chatbot
+        config={config}
+        messageParser={MessageParser}
+        actionProvider={ActionProvider}
+      />
     </div>
   );
 }
--- react-chatbot/src/config.js
+++ react-chatbot/src/config.js
@@ -1,5 +1,18 @@
+import { createChatBotMessage } from 'react-chatbot-kit';
+
+const botName = 'Lourdene Buendia';
+
 const config = {
-    initialMessages: [createChatBotMessage(`Hello world`)]
-  }
-  
-  export default config
+  initialMessages: [createChatBotMessage(`Hi! I'm ${botName}`)],
+  botName: botName,
+  customStyles: {
+    botMessageBox: {
+      backgroundColor: '#376B7E',
+    },
+    chatButton: {
+      backgroundColor: '#5ccc9d',
+    },
+  },
+};
+
+export default config;
